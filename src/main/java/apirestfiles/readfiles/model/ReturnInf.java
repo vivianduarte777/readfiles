7ded5d0be4cdf1868b97f316a847cbf902ff22c9
vivian <vividua@gmail.com>
2021-07-11 17:01:43 -0300

    Read  files new version

--- src/main/java/apirestfiles/readfiles/model/ReturnInf.java
+++ src/main/java/apirestfiles/readfiles/model/ReturnInf.java
@@ -1,25 +1,26 @@
 package apirestfiles.readfiles.model;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 public class ReturnInf {
     private String information;
     private String urlAddress;
-
+    @Bean
     public String getUrlAddress() {
         return urlAddress;
     }
-
+    @Bean
     public void setUrlAddress(String urlAddress) {
         this.urlAddress = urlAddress;
     }
 
-    @ResponseBody
+    @Bean
     public String getFileInformation() {
         return information;
     }
-
+    @Bean
     public void setFileInformation(String fileInformation) {
         this.information = fileInformation;
     }
